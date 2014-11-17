449c288a8ce70dffcd9d9de7415b3772abbe09a8
Guillaume Pinot <texitoi@texitoi.eu>
2014-11-17 14:09:53 +0100

    correctly quote the query

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,19 @@ fn index_bano(files: &[String]) {
     }
 }
 
-docopt!(Args deriving Show, "
-Usage:
-    munin index <bano-files>...
-    munin query <query>
-    munin geocode [--] <lon> <lat>
-", arg_lon: Option<f64>, arg_lat: Option<f64>)
+fn to_json_string(s: &str) -> String {
+    use serialize::json;
+
+    let mut w = std::io::MemWriter::with_capacity(s.len() + 2);
+
+    // as `s` is a valid utf8 string, and we use MemWriter, these
+    // unwrap can't fail
+    json::escape_bytes(&mut w, s.as_bytes()).unwrap();
+    String::from_utf8(w.unwrap()).unwrap()
+}
 
 fn query(q: &str) -> Result<(), curl::ErrCode> {
-    let query = format!(include_str!("../json/query.json"), query=q);
+    let query = format!(include_str!("../json/query.json"), query=to_json_string(q)[]);
     println!("{}", query);
     let r = try! {
         curl::http::handle().post("http://localhost:9200/munin/_search?pretty", query[])
@@ -51,6 +55,13 @@ fn geocode(lon: f64, lat: f64) -> Result<(), curl::ErrCode> {
     println!("{}", r);
     Ok(())
 }
+
+docopt!(Args deriving Show, "
+Usage:
+    munin index <bano-files>...
+    munin query <query>
+    munin geocode [--] <lon> <lat>
+", arg_lon: Option<f64>, arg_lat: Option<f64>)
 
 fn main() {
     let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
